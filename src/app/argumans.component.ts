8a2f51c14f57d770f59ecc705915eceeb5f908fa
Thoi Duong <dnhthoi@gmail.com>
2016-08-18 14:54:25 +0700

    add: routing to Arguman and navigate

--- src/app/argumans.component.ts
+++ src/app/argumans.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import {Arguman} from './arguman';
 import { ArgumanService }  from './arguman.service';
 @Component({
@@ -6,13 +7,31 @@ import { ArgumanService }  from './arguman.service';
   styleUrls: ['app/argumans.component.css'],
   templateUrl: 'app/argumans.component.html'
 })
-export class ArgumansComponent {
+export class ArgumansComponent implements OnInit {
   argumans: Arguman[];
-  constructor(argumanService: ArgumanService) {
-    this.argumans = argumanService.getArgumans();
+  constructor( private argumanService: ArgumanService, private route: ActivatedRoute, private router: Router) {
   }
-  selectedTopic: Arguman;
-  onSelect(topic: Arguman) { this.selectedTopic = topic; console.log(topic.content); }
+  ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      if (params['id'] !== undefined) {
+        let id = +params['id'];
+        this.getArgumans(id);
+      } else {
+        console.log("Undefined params!!!!!!" + params['id']);
+      }
+    });
+    // this.getArgumans();
+  }
+  getArgumans(id: number){
+    this.argumanService.getArgumans(id).then(argumans => {this.argumans = argumans; console.log('arguman length::' + this.argumans.length);});
+  }
+  selectedAaguman: Arguman;
+  onSelect(arguman: Arguman) { this.selectedAaguman = arguman; console.log(arguman.content); }
+
+  gotoArguman(arguman: Arguman) {
+    let link = ['/arguman', arguman.id];
+    this.router.navigate(link);
+  }
 }
 
 
