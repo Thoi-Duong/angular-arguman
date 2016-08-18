ae6459c5ee16e94e8f38a7ebe2339c419591eae4
Thoi Duong <dnhthoi@gmail.com>
2016-08-18 15:11:13 +0700

    add routing to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing }       from './app.routing';
 import { AppComponent }  from './app.component';
 import { ArgumansComponent }  from './argumans.component';
 import { ArgumanService }  from './arguman.service';
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, routing ],
   declarations: [ ArgumansComponent, AppComponent ],
   bootstrap: [ AppComponent ],
   providers: [ ArgumanService ]
