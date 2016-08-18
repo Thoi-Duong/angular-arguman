4d767704cefbc4a5d2960805c614e169ad363eae
Thoi Duong <dnhthoi@gmail.com>
2016-08-18 14:46:41 +0700

    add more dum data

--- src/app/mock-argumans.ts
+++ src/app/mock-argumans.ts
@@ -5,7 +5,12 @@ export var ARGUMANS: Arguman[] = [
   {id: 12, type: 'but', content: 'Không ăn thịt chó thể hiện sự nhân đạo', belong: 11},
   {id: 13, type: 'but', content: 'Ăn thịt chó đi ngược lại xu hướng đám đông', belong: 11},
   {id: 14, type: 'but', content: 'Chó là loài vật trung thành nhất', belong: 11},
-  {id: 15, type: 'but', content: 'Chó là bạn chứ không phải đồ ăn', belong: 11},
-  {id: 16, type: 'because', content: 'tại sao bạn cho phép được ăn thịt cá, thịt chim mà không cho phép ăn thịt chó', belong: 11},
-  {id: 17, type: 'however', content: 'Sở thích ăn uống là chuyện của mỗi người, chẳng có ai cấm cả', belong: 11}
+  {id: 15, type: 'but', content: 'Chó là bạn chứ không phải đồ ăn', belong: 12},
+  {id: 16, type: 'because', content: 'tại sao bạn cho phép được ăn thịt cá, thịt chim mà không cho phép ăn thịt chó', belong: 12},
+  {id: 17, type: 'however', content: 'Sở thích ăn uống là chuyện của mỗi người, chẳng có ai cấm cả', belong: 12},
+  {id: 13, type: 'but', content: 'Ăn thịt chó đi ngược lại xu hướng đám đông', belong: 13},
+  {id: 14, type: 'but', content: 'Chó là loài vật trung thành nhất', belong: 13},
+  {id: 15, type: 'but', content: 'Chó là bạn chứ không phải đồ ăn', belong: 13},
+  {id: 16, type: 'because', content: 'tại sao bạn cho phép được ăn thịt cá, thịt chim mà không cho phép ăn thịt chó', belong: 14},
+  {id: 17, type: 'however', content: 'Sở thích ăn uống là chuyện của mỗi người, chẳng có ai cấm cả', belong: 14}
 ];
