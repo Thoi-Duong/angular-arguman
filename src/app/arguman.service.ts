f8d63355ad257567ba51c171ed04b3dc05b1b569
Thoi Duong <dnhthoi@gmail.com>
2016-08-18 15:16:55 +0700

    filter arguman with id

--- src/app/arguman.service.ts
+++ src/app/arguman.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ARGUMANS }     from './mock-argumans';
 import { Arguman } from './arguman';
+import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class ArgumanService {
-  getArgumans() {
-    return ARGUMANS;
+  getArgumans(id: number) {
+    console.log('Arguman id::: '+id);
+    var agus: Arguman[] = ARGUMANS.filter(function(arguman){
+       return arguman.belong === id;
+    }) as Arguman[];
+    return Promise.resolve(agus);
   }
+
+  // getArgunamChilds(id: number){
+  //   this.getArgumans().map((arguman) => heroes.find(hero => hero.id === id));
+  // }
 }
